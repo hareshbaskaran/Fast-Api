bfe4eb25f105b5dd9bc41cbda79320e873119064
hareshbaskaran <baskaranharesh@gmail.com>
2023-02-24 16:12:25 +0530

    parameters - complex subtypes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from router1 import get_blog
-
+from router1 import post_plog
 
 app = FastAPI()
 app.include_router(get_blog.router)
-
+app.include_router(post_plog.router)
 @app.get('/hello')
 def index():
   return {'message': 'Hello world!'}
