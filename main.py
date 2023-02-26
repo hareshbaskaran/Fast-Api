093d47c2d7679644dbddf79dd15c62da104b6440
hareshbaskaran <baskaranharesh@gmail.com>
2023-02-26 10:30:08 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from typing import Optional
 from fastapi import FastAPI
 from router1 import get_blog
 from router1 import post_plog
+from router1 import user
+from router1 import article
+from db.database import engine
+from db import models
+
 
 app = FastAPI()
+app.include_router(user.router)
+app.include_router(article.router)
 app.include_router(get_blog.router)
 app.include_router(post_plog.router)
+
 @app.get('/hello')
 def index():
   return {'message': 'Hello world!'}
+
+models.Base.metadata.create_all(engine)
